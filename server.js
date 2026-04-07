5522e96a51945a5f69d97109ff34150064538c74
prithaxdev <karkiparas020@gmail.com>
2026-04-07 20:22:39 +0545

    feat: add MongoDB connection module and validate MONGO_URI on startup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import app from "./src/app.js";
+import { connectDB } from "./src/config/database.js";
 
 const port = 3000;
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI environment variable is not set");
+}
+
 const config = {
   MONGO_URI: process.env.MONGO_URI,
 };
